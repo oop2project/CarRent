70305e63bae0ac14218fed867f7ebf4c3b0d8940
PavelDStoyanov <161254416+PavelDStoyanov@users.noreply.github.com>
2024-11-06 12:36:06 +0200

    Enums

--- CarRent/src/main/java/models/Car.java
+++ CarRent/src/main/java/models/Car.java
@@ -1,5 +1,12 @@
 package models;
 
+import enums.CarCategory;
+import enums.CarClass;
+import enums.ReturnStatus;
+
 public class Car {
     private double price;
+    private CarCategory carCategory;
+    private CarClass carClass;
+    private ReturnStatus returnStatus;
 }
